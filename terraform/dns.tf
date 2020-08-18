97022e3899f68601ff3ccdb82c367af29a0b9650
localuser <localuser@borat.culturallearnings.us>
2020-08-18 14:32:41 +0200

    FIX \terraform\dns.tf FIX \terraform\provider.tf

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -1,7 +1,7 @@
 /* resource "cloudflare_record" "dns_a_services" {
   zone_id = var.dns_zone_id
   name    = "services.${var.dns_domain}"
-  value   = module.ignition.ipv4_addresses[0]
+  value   = module.services.ipv4_addresses[0]
   type    = "A"
   ttl     = 1
   count   = var.bootstrap == true ? 1 : 0
